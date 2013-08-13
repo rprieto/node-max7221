800f606d56b0c9228fb35874e9ab32a1a8b90900
Romain Prieto <romain.prieto@gmail.com>
2013-08-13 23:36:18 +1000

    Code samples

---
+++ examples/helloworld.js
@@ -0,0 +1,15 @@
+var max7221 = require('../max7221');
+
+max7221.configure({
+    dataIn: 22,
+    load: 24,
+    clock: 26
+});
+
+max7221.open(function() {
+    max7221.scroll(max7221.text('hello world!'));
+});
+
+process.on('SIGTERM', function() {
+    max7221.close();
+});
